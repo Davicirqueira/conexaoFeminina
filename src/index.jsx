ca91ea2d9976b43737158aa5591c06f4d4958022
davi <daviforever17@gmail>
2024-10-28 10:39:08 -0300

    cf

---
+++ src/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import Cf from './pages';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Cf />
+  </React.StrictMode>
+);
---
+++ src/pages/index.jsx
@@ -0,0 +1,26 @@
+import './index.scss';
+
+export default function Cf() {
+
+  return (
+
+    <div className="pageCf">
+
+      <header>
+
+        <div>
+
+          <img src="" alt="" />
+
+        </div>
+
+        
+
+      </header>
+
+    </div>
+
+  );
+}
+
+
